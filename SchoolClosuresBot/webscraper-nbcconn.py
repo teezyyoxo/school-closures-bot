f8b5ed6e383f2d68b829b86550e6ce31ec07b9e8
teezyyoxo <teezyyoxo@users.noreply.github.com>
2024-12-16 23:05:11 -0500

    appended "-nbcconn" to script name, added test mode. uses the archive.org snapshot from 12/12/24 when script is executed with "-t"

--- SchoolClosuresBot/webscraper-nbcconn.py
+++ SchoolClosuresBot/webscraper-nbcconn.py
@@ -1,23 +1,20 @@
-# i can't figure out the libreSSL nonsense. error doesn't show when run with python3.9, but does with python3.
-# so, the below will suppress the error messaging:
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module='urllib3')
-# phew.
-
 import requests
 from bs4 import BeautifulSoup
+import argparse
+
+# i can't figure out the libreSSL nonsense. error doesn't show when run with python3.9, but does with python3.
+# so, the below will suppress the error messaging:
+warnings.filterwarnings("ignore", category=UserWarning, module='urllib3')
 
 # we're using NBC Connecticut for this one, so the URL is usually this: https://www.nbcconnecticut.com/weather/school-closings/
 # but there are no closings currently, so i have no way to test. so i'm using an archived page from a few days ago for testing.
 # MAKE SURE TO CHANGE THIS!
-school_closures_url = "https://web.archive.org/web/20241212072827/https://www.nbcconnecticut.com/weather/school-closings/"
+live_school_closures_url = "https://www.nbcconnecticut.com/weather/school-closings/"
+test_school_closures_url = "https://web.archive.org/web/20241212072827/https://www.nbcconnecticut.com/weather/school-closings/"
 
 # Function to fetch school closures
-def fetch_school_closures():
-    url = school_closures_url  # Use the variable here
-    # Your code for fetching the school closures goes here
-
-    
+def fetch_school_closures(url):
     # Fetch the page content
     response = requests.get(url)
     
@@ -44,8 +41,21 @@ def fetch_school_closures():
 
     return school_data
 
-# Example usage
+# Main execution
 if __name__ == "__main__":
-    closures = fetch_school_closures()
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="Fetch school closures from NBC Connecticut.")
+    parser.add_argument(
+        "-t", "--test", 
+        action="store_true", 
+        help="Run the script in test mode using an archived URL."
+    )
+    args = parser.parse_args()
+
+    # Determine which URL to use
+    school_closures_url = test_school_closures_url if args.test else live_school_closures_url
+
+    # Fetch and display school closures
+    closures = fetch_school_closures(school_closures_url)
     for closure in closures:
         print(f"{closure['school']}: {closure['status']}")
